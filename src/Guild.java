2ed416ae0a60c1f919f75b749ceb46073746e4bc
skubidubudu1 <wojtek.adamczyk99@gmail.com>
2022-12-16 22:21:24 +0100

    whatever

--- src/Guild.java
+++ src/Guild.java
@@ -1,12 +1,31 @@
 public class Guild extends Building
 {
-    public void Guild1()
-    {
+    City falkreath;
+    City whiterun;
+    City winterhold;
+    City riften;
 
+    public void Guild(City falkreath, City whiterun, City winterhold, City riften)
+    {
+        this.falkreath = falkreath;
+        this.whiterun = whiterun;
+        this.winterhold = winterhold;
+        this.riften = riften;
+
+//        switch ()
+//        {
+//            case falkreath ->
+//            {
+//
+//            }
+//
+//            case whiterun ->
+//            {
+//
+//            }
+//
+//        }
     }
 
-    public void Guild2()
-    {
 
-    }
 }
